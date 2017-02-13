06786fb2e11e0cd44125738c0594f7996f37f106
Daniel Shmuglin <daniels@wix.com>
2017-02-13 15:44:06 +0200

    Heap skeleton

---
+++ src/main/java/com/wix/exaps/heap/Heap.java
@@ -0,0 +1,58 @@
+package com.wix.exaps.heap;
+
+import java.util.Set;
+
+public class Heap {
+
+    private static final int DEFAULT_HEAP_CAPACITY = 50;
+
+    private static final int DEFAULT_OBJECT_SIZE = 10;
+
+    public static Heap empty(int capacity) {
+        //TODO: implement
+        return null;
+    }
+
+    public static Heap empty() {
+        return empty(DEFAULT_HEAP_CAPACITY);
+    }
+
+    public Reference allocateRoot(int size) {
+        //TODO: implement
+        return null;
+    }
+
+    public void gc(boolean compact) {
+        //TODO: implement
+    }
+
+
+    public void gc() {
+        gc(true);
+    }
+
+    public Reference allocateRoot() {
+        return allocateRoot(DEFAULT_OBJECT_SIZE);
+    }
+
+    public Reference allocate(int size) {
+        //TODO: implement
+        return null;
+    }
+
+    public Reference allocate() {
+        return allocate(DEFAULT_OBJECT_SIZE);
+    }
+
+    public Set<Reference> references() {
+        //TODO: implement
+        return null;
+    }
+
+    public static class OutOfMemory extends RuntimeException {
+
+        OutOfMemory() {
+            super("No more space left in the heap");
+        }
+    }
+}
